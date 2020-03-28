45cbc4b5d3d7fdc8917fa41836e86c7e03d3acea
ahmedadams <hayaanmow@gmail.com>
2020-03-28 15:44:44 +0000

    push this update

--- database/factory.js
+++ database/factory.js
@@ -11,11 +11,13 @@
 |
 */
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+/** @type {import('@adonisjs/lucid/src/Factory')} */ 
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', (faker) => {
+  return {
+    username: faker.username(),
+    email: faker.email(),
+    password: faker.string({ length: 8 })
+  }
+})
