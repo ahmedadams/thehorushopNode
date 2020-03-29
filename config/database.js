664ce8116dfe70d7e364f83b59243bf3b0eef041
ahmedadams <hayaanmow@gmail.com>
2020-03-29 19:24:37 +0100

    updated database.js file configs for mysql

--- config/database.js
+++ config/database.js
@@ -52,11 +52,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
+      host: Env.get('DB_HOST', '127.0.0.1'),
+      port: Env.get('DB_PORT', '3306'),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'horus')
     }
   },
 
